c5c2eea0ad4e06ceca4ba986b38de727f90e3289
codesktop <codesktop@foxmail.com>
2021-10-23 20:00:06 +0800

    init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/components/**/*.vue',
+    './src/layouts/**/*.vue',
+    './src/pages/**/*.vue'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
