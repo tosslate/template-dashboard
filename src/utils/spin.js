024190977184beb65412997ed1383486bc005c76
tossode <tossdev@189.cn>
2022-07-15 23:38:38 +0800

    reset repository

--- src/utils/spin.js
+++
@@ -1,7 +0,0 @@
-export function hiddenSpin(identifier) {
-  const spinId = identifier.replace(/#/, '')
-  const spinEl = document.getElementById(spinId)
-
-  spinEl.classList.add('hidden')
-  return spinEl
-}
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.jsx' // prettier-ignore
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
